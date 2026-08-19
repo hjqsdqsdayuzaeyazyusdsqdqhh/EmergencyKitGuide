19ae3db67eff526292f4852507958c05a1642d45
Khalid <adimozada463@gmail.com>
2026-08-19 22:37:48 +0100

    Update car kit affiliate URLs sitewide

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var AFFILIATE = {
-  carKit: 'https://www.catrunlu.com/5LLufo4FjTH8IAnydMHUmLx-gYvluYaA0RQkH04RLOrDB-fHaUv2H9PZR8nvPQajsLZ21eV8v9ky22HzNanNjw~~/1st/2st',
+  carKit: 'https://www.catrunlu.com/5LLufo4FjTH8IAnydMHUmJXVioXuHAZGqG7VoltE0kajG58aCFIJNNYoCj8__hz1T5sINYjn888AIjXadq8vPA~~/szz/szzz',
   fireKit: 'https://www.catrunlu.com/5LLufo4FjTH8IAnydMHUmIkUtq82Ou10Y7fYzVH07a_d3WtUW9JOdFSycvlYghTXUTXJAyht43n9GfY5qdDoEA~~/1se'
 };
